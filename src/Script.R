ade588e927a49953b192a7d73c45488d0d892262
Mattia Franchini <mattiafranchini@MacBook-Air-di-Frank.local>
2025-12-01 12:34:50 +0100

    Created R script file

---
+++ src/Script.R
@@ -0,0 +1,7 @@
+# =========================================================
+# Project: Agrimonia Air Quality Regression
+# Description: Main R script for the project
+# Authors: Mattia Franchini, Michele Bisignano, Lino Mazzoleni, Fabio Ghislanzoni
+# Affiliation: University of Bergamo, Computer Engineering
+# Academic Year: 2025/2026
+# =========================================================
